a35c9ea2f921f8180bbad5559b5415b2f3a99039
Nilton <hajikazuo@outlook.com>
2023-08-01 07:57:24 -0300

    Add project files.

---
+++ MvcWebIdentity/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcWebIdentity.Entities;
+
+namespace MvcWebIdentity.Context
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    AlunoId = 1,
+                    Nome = "Nilton",
+                    Email = "hajikazuo@outlook.com",
+                    Idade = 24,
+                    Curso = "Programação"
+                });
+        }
+    }
+}
